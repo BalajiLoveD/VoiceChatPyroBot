eb84dae0bc81279e17bba9290493db5c6e6757ab
BalajiLoveD <62749324+BalajiLoveD@users.noreply.github.com>
2021-01-01 16:45:57 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -3,21 +3,21 @@ from pyrogram import filters
 from helpers import get_banned_users
 
 # Get these two from https://my.telegram.org
-API_ID = 1234567
-API_HASH = "ab1c23def45fg67890h123i45678j9kl"
+API_ID = 1366324
+API_HASH = "0d0274833b354d9223ca60b3e2f1e7c8"
 
 # Get this from @Botfather
-TOKEN = "1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI"
+TOKEN = "1437099346:AAGyH2twmU12MW3EXtYarNCJ4c0vkx1FloQ"
 
 # The IDs of the users which can stream, skip, pause and change volume
 SUDO_USERS = [
-    383407735,
-    951435494,
-    1392620345
+    829327264,
+    1356608073,
+    1302101407,
 ]
 
 # A group ID to send messages to when a song starts playing
-# Example group ID: -1001402753006
+# Example group ID: -1001477743244
 LOG_GROUP = None  # Just keep it like this if you are not going to use one
 
 # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
@@ -27,7 +27,7 @@ LANG = "en"
 DUR_LIMIT = 5
 
 # Show a small credit for @su_Bots in the start message
-CREDIT = True
+CREDIT = False
 
 # No need to touch the following.
 SUDO_FILTER = filters.user(SUDO_USERS)
